86f1270bd76da9b5ab4f70e6c4ba926f2ae90309
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2018-04-26 15:02:59 +0300

    Update Travis scripts from https://github.com/mmasdeu/sage_package_template

--- .travis-test.sh
+++ .travis-test.sh
@@ -1,4 +1,15 @@
-#! /bin/sh
+#!/bin/sh
 set -e
-make check-long SAGE="$HOME/SageMath/sage" SAGE_CHECK_FLAGS="--verbose"
-make doc SAGE="$HOME/SageMath/sage"
+if [ "${SAGE_AGE}" = "-1" ]; then
+    sudo sage -pip install --user --upgrade -v -i https://pypi.python.org/pypi sagemath
+    sudo sage -pip install --upgrade --no-index -v .
+    sudo sage ./setup.py test
+    (cd docs && sudo sage -sh -c "make html")
+    sudo sage -pip uninstall .
+else
+    ${HOME}/SageMath/sage -pip install --upgrade -v -i https://pypi.python.org/pypi sagemath
+    ${HOME}/SageMath/sage -pip install --upgrade --no-index -v .
+    ${HOME}/SageMath/sage setup.py test
+    (cd docs && ${HOME}/SageMath/sage -sh -c "make html")
+    ${HOME}/SageMath/sage -pip uninstall .
+fi
