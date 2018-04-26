9a97f7808bcdd8ac6ce395b51f2afb4e11ab1d3a
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2018-04-26 15:58:13 +0300

    Reduce test verbosity

--- .travis-test.sh
+++ .travis-test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
-make check-long SAGE="$HOME/SageMath/sage" SAGE_CHECK_FLAGS="--verbose"
+make check-long SAGE="$HOME/SageMath/sage" #SAGE_CHECK_FLAGS="--verbose"
 make doc SAGE="$HOME/SageMath/sage"
