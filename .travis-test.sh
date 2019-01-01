5cebbd60495fa7f789c2941b6d278eff42aaca37
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2018-12-31 17:53:26 -0800

    Fixup

--- .travis-test.sh
+++ .travis-test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
-$HOME/SageMath/sage -pip install --upgrade --no-index -v .
+$HOME/SageMath/sage -pip -vvv install --upgrade .
 make check-long SAGE="$HOME/SageMath/sage" #SAGE_CHECK_FLAGS="--verbose"
 make doc SAGE="$HOME/SageMath/sage"
