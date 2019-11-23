83c11054c3d261feb5d57338ec1c552cf6fdc210
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2019-11-22 23:16:20 -0500

    docs/source/conf.py: Put package folder first in search path so it wins over an installed version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,7 @@ except ImportError:
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.append(os.path.abspath(package_folder))
+sys.path.insert(0, os.path.abspath(package_folder))
 sys.path.append(os.path.join(SAGE_SRC, "sage_setup", "docbuild", "ext"))
 
 
