931a0651efd6ebf5cd5bd3d70c914d43e3ea619f
Travis CI <nobody@example.org>
2019-11-23 02:38:33 +0000

    Automatic upload of documentation built from 7bc8929117449ff71851b8095fe15552d688bb75

--- doc/html/extreme_functions-5.py
+++ doc/html/extreme_functions-5.py
@@ -1,4 +1,5 @@
 from cutgeneratingfunctionology.igp import *
-h = bcds_discontinuous_everywhere()
-g = h.plot(show_legend=False, aspect_ratio=0.125, figsize=(8, 1.5), thickness=2, ticks=[[QQ('1/2'), 1],[1]], tick_formatter=[[r'$f=\frac{1}{2}$', "$1$"], ["$1$"]])
+h = bccz_counterexample()
+h = psi_n_in_bccz_counterexample_construction(e=generate_example_e_for_psi_n(n=7))
+g = plot(h, show_legend=False, aspect_ratio=0.125, figsize=(8, 1.5), thickness=1, **only_f_ticks_keywords(h))
 sphinx_plot(g)
--- doc/html/extreme_functions-64.py
+++ doc/html/extreme_functions-64.py
@@ -1,4 +1,4 @@
 from cutgeneratingfunctionology.igp import *
-h = mlr_cpl3_g_3_slope()
+h = mlr_cpl3_f_2_or_3_slope()
 g = plot_with_colored_slopes(h, show_legend=False, aspect_ratio=0.125, figsize=(8, 1.5), thickness=2, **only_f_ticks_keywords(h))
 sphinx_plot(g)
