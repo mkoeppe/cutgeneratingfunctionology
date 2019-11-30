c7591d62100fca66f3e2384a73ee52bc4abfa23c
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2019-11-30 14:42:19 -0500

    .travis-quick-test.sh: Use pip install --upgrade-strategy only-if-needed

--- .travis-quick-test.sh
+++ .travis-quick-test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
-sage -pip -vvv install --upgrade -r requirements.txt || pip -vvv install --upgrade -r requirements.txt
+sage -pip -vvv install --upgrade-strategy only-if-needed -r requirements.txt || pip -vvv install --upgrade-strategy only-if-needed -r requirements.txt
 make check
