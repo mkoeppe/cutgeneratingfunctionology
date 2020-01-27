9d1798c91fe40cd09742d983abbec42a2c06b0e8
Yuan Zhou <yzh@ucdavis.edu>
2020-01-26 23:24:27 -0500

    new BasicSemialgebraicSet_mathematica.remove_redundant_constraints

--- cutgeneratingfunctionology/spam/semialgebraic_mathematica.py
+++ cutgeneratingfunctionology/spam/semialgebraic_mathematica.py
@@ -6,13 +6,13 @@ from __future__ import division, print_function, absolute_import
 
 from cutgeneratingfunctionology.spam.basic_semialgebraic import  BasicSemialgebraicSet_eq_lt_le_sets
 from sage.interfaces.mathematica import mathematica
+import operator
 
 class BasicSemialgebraicSet_mathematica(BasicSemialgebraicSet_eq_lt_le_sets):
     """
     EXAMPLES::
 
         sage: from cutgeneratingfunctionology.spam.semialgebraic_mathematica import BasicSemialgebraicSet_mathematica
-        sage: import operator
         sage: Q.<x,y,z> = QQ[]
         sage: bsa = BasicSemialgebraicSet_mathematica(QQ, poly_ring=Q)
         sage: bsa.add_polynomial_constraint(z-1, operator.eq)
@@ -26,6 +26,14 @@ class BasicSemialgebraicSet_mathematica(BasicSemialgebraicSet_eq_lt_le_sets):
         'z - 1 == 0 && x^2 + y^2 - 4 < 0 && -y + 1 <= 0'
         sage: bsa.find_point()  # optional - mathematica
         (0, 3/2, 1)
+        sage: bsa.add_polynomial_constraint(y-x^2/3, operator.gt)
+        sage: bsa.add_polynomial_constraint(x-3, operator.le)
+        sage: bsa.add_polynomial_constraint(x+1, operator.ge)
+        sage: bsa
+        BasicSemialgebraicSet_mathematica(eq=[z - 1], lt=[1/3*x^2 - y, x^2 + y^2 - 4], le=[-y + 1, -x - 1, x - 3])
+        sage: bsa.remove_redundant_constraints()  # optional - mathematica
+        sage: bsa                                 # optional - mathematica
+        BasicSemialgebraicSet_mathematica(eq=[z - 1], lt=[x^2 + y^2 - 4], le=[-y + 1, -x - 1])
 
     Test that the underscores have been replaced properly::
 
@@ -54,6 +62,9 @@ class BasicSemialgebraicSet_mathematica(BasicSemialgebraicSet_eq_lt_le_sets):
     #     pass # TODO: check is_polynomial_constraint_valid first. Worth doing?
 
     def find_point(self):
+        """
+        Use Mathematica's ``FindInstance``
+        """
         pt_math = mathematica.FindInstance(self.constraints_string(), self.variables_string())  # For the first doctest, pt_math is of the form {{x -> 0, y -> 3/2, z -> 1}}.
         if len(pt_math) == 0:
             return None
@@ -82,3 +93,39 @@ class BasicSemialgebraicSet_mathematica(BasicSemialgebraicSet_eq_lt_le_sets):
         for l in self.le_poly():
             constr += str(l) + ' <= 0 && '
         return constr[:-4].replace("_", "@")
+
+    def remove_redundant_constraints(self):
+        r"""
+        Use Mathematica's ``FindInstance`` to discard redundant inequalities or equations in the bsa description. Update self._eq, self._lt and self._le. The result is order dependent.
+        """
+        eqs = sorted(self.eq_poly())
+        lts = sorted(self.lt_poly())
+        les = sorted(self.le_poly())
+        eq = []; lt = []; le = []
+        for i in range(len(les)):
+            bsa = BasicSemialgebraicSet_mathematica(eq=eqs, lt=lts, le=le+les[i+1::])
+            bsa.add_polynomial_constraint(les[i], operator.gt)
+            pt = bsa.find_point()
+            if pt is not None:
+                le.append(les[i])
+        for i in range(len(lts)):
+            bsa = BasicSemialgebraicSet_mathematica(eq=eqs, lt=lt+lts[i+1::], le=le)
+            bsa.add_polynomial_constraint(lts[i], operator.ge)
+            pt = bsa.find_point()
+            if pt is not None:
+                lt.append(lts[i])
+        for i in range(len(eqs)):
+            bsa = BasicSemialgebraicSet_mathematica(eq=eq+eqs[i+1::], lt=lt, le=le)
+            bsa.add_polynomial_constraint(eqs[i], operator.lt)
+            pt = bsa.find_point()
+            if pt is not None:
+                eq.append(eqs[i])
+            else:
+                bsa = BasicSemialgebraicSet_mathematica(eq=eq+eqs[i+1::], lt=lt, le=le)
+                bsa.add_polynomial_constraint(eqs[i], operator.gt)
+                pt = bsa.find_point()
+                if pt is not None:
+                    eq.append(eqs[i])
+        self._eq = set(eq)
+        self._lt = set(lt)
+        self._le = set(le)
