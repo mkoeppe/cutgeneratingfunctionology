c2800fc2e77e76637f371e138a2936dd7a082af1
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2024-12-25 19:50:03 -0800

    setup.py: Clean up

--- setup.py
+++ setup.py
@@ -1,14 +1,4 @@
-## -*- encoding: utf-8 -*-
-import os
-import sys
 from setuptools import setup
-from codecs import open # To open the README file with proper encoding
-
-
-# Get information from separate files (README, VERSION)
-def readfile(filename):
-    with open(filename,  encoding='utf-8') as f:
-        return f.read()
 
 setup(
     packages = ['cutgeneratingfunctionology', 'cutgeneratingfunctionology.igp', 'cutgeneratingfunctionology.multirow', 'cutgeneratingfunctionology.dff', 'cutgeneratingfunctionology.spam', 'cutgeneratingfunctionology.igp.subadditivity_slack_diagrams', 'cutgeneratingfunctionology.igp.procedures'],
